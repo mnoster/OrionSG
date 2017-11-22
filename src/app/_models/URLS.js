37d7cd9236baf9277ef8f35ffd33b8e58214f7eb
Nicholas Porter <njporter10@gmail.com>
2017-11-21 23:18:03 -0800

    Added in the new enpoint IP value

--- src/app/_models/URLS.js
+++ src/app/_models/URLS.js
@@ -1,4 +1,4 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.DEV_URL = "http://localhost:7788";
+exports.DEV_URL = "http://54.187.154.6";
 //# sourceMappingURL=urls.js.map
