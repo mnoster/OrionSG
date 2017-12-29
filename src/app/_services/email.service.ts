acc78a615930325c6f4203f9be0970945bf5333e
Nicholas Porter <nicholas@metax.io>
2017-12-29 15:12:23 -0800

    added in the new emailer

--- src/app/_services/email.service.ts
+++ src/app/_services/email.service.ts
@@ -9,9 +9,9 @@ export class EmailService {
     constructor(private http:Http) {}
     config:any = {headers:{ 'Content-Type': 'application/json'}};
 
-    emailApplicant(email: string, job_title: string, url: string) {
+    emailApplicant(email: string, job_title: string, url: string, applicant_data:any, resume:any) {
         let promise = new Promise((resolve, reject) => {
-            this.http.post(DEV_URL + "/email", {email, job_title, url}, this.config)
+            this.http.post(DEV_URL + "/email", {email, job_title, url, applicant_data, resume}, this.config)
                 .toPromise()
                 .then(
                     (res) => {
