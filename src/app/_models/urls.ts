5bdfc41ec2c34ea14021446acbfabe77a216aa3d
sdinay <sdinay@yahoo.com>
2017-12-29 16:42:39 -0800

    working on our team

--- src/app/_models/urls.ts
+++ src/app/_models/urls.ts
@@ -1,2 +1,2 @@
-export const DEV_URL =  "http://54.187.154.6";
-//export const DEV_URL =  "http://localhost:7788";
+export const DEV_URL =  "https://54.187.154.6";
+//export const DEV_URL =  "https://localhost:7788";
--- src/app/ourteam/ourteam.component.ts
+++ src/app/ourteam/ourteam.component.ts
@@ -15,6 +15,32 @@ export class OurteamComponent implements OnInit {
     currentUser: User;
     users: User[] = [];
 
+    summaryKrisFlynn1 = "Kris brings more than 15 years experience in client "
+    + "service, project management, and IT services. In addition to working "
+    + "directly with our clients, Kris also acts as our Chief Operating "
+    + "Officer, handling all our internal infrastructure and go-to market "
+    + "capabilities. ";
+
+    summaryKrisFlynn2 = "Kris earned her BA in Finance and MBA in Supply Chain "
+    + "Management from Michigan State University. Prior to Orion, Kris led "
+    + "global development teams for the production order management systems of "
+    + "a major automotive OEM.";
+
+    summaryDaveFlynn1 = "Dave brings more than 20 years of experience delivering "
+    + "client solutions across a variety of industries including automotive, "
+    + "banking, insurance, and government. In addition to managing consulting "
+    + "and staffing practices, Dave is a hands-on leader with a broad range of "
+    + "project experience across industries and functional areas. ";
+
+    summaryDaveFlynn2 = "Dave earned his BA in accounting and his MBA from Michigan "
+    + "State University. Dave has earned the CPA and CISA designations, and "
+    + "sits on the boards of Junior Achievement of Southeastern Michigan and the "
+    + "Detroit Chapter of the Institute of Internal Auditors.";
+
+    summaryDaveFlynn3 = "In his role as President, Dave is responsible for the "
+    + "performance of all our service lines and has direct management "
+    + "responsibility for our consulting and staffing business.";
+
     constructor(private userService: UserService) {
         this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
     }
