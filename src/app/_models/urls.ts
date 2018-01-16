f535e6c49b35a9b54b520b1729736ec2c8db2612
Nicholas Porter <nicholas@metax.io>
2018-01-15 19:59:01 -0800

    Added in the api urls

--- src/app/_models/urls.ts
+++ src/app/_models/urls.ts
@@ -1,2 +1,2 @@
-export const DEV_URL =  "https://54.187.154.6";
+export const DEV_URL =  "http://orionsolutionsgroup-back.us-west-2.elasticbeanstalk.com";
 //export const DEV_URL =  "https://localhost:7788";
