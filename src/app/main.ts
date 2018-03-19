e5d77e0c813069b99177d7b1e0e1ef70f00b471d
sdinay <sdinay@yahoo.com>
2018-03-19 14:17:48 -0700

    changed to prod mode and updated loading screen to white

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,5 +1,9 @@
 ﻿import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app.module';
+import {enableProdMode} from '@angular/core';
 
+if (environment.production) {
+  enableProdMode();
+}
 platformBrowserDynamic().bootstrapModule(AppModule);
