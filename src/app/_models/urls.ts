d70d2f39449b14817d30b159f965cddcfa4b15f9
Nicholas Porter <nicholas@metax.io>
2018-05-02 20:29:26 -0700

    The temporaary cert on load balancer is no longer valid

--- src/app/_models/urls.ts
+++ src/app/_models/urls.ts
@@ -1,3 +1,3 @@
 // export const _URL =  "http://orion-api.us-west-2.elasticbeanstalk.com";
 // export const _URL =  "http://localhost:7788";
-export const _URL = "https://prod.osg-api.net";
+export const _URL = "http://prod.osg-api.net";
