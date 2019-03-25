a78b20dd5c476f8e462bf131cba8e8d6ac4463bd
Nicholas Porter <nicholas@metax.io>
2019-03-25 09:13:09 -0700

    Updated the suite number

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -20,7 +20,7 @@ export class ContactComponent {
 
     locations = [
         {
-            address: "2950 West Square Lake Road, Suite 100<br> Troy, MI, USA <br> 48098",
+            address: "2950 West Square Lake Road, Suite 202<br> Troy, MI, USA <br> 48098",
             phone: "(855) 223-4020",
             name: "Corporate Headquarters: Troy",
             mapUrl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11746.543711135988!2d-83.21590863221654!3d42.60546737917119!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8824c11fdd241e0b%3A0x47bf9db70c91363a!2s2950+W+Square+Lake+Rd+%23100%2C+Troy%2C+MI+48098!5e0!3m2!1sen!2sus!4v1510104739436"
