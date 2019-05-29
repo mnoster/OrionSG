c72c1172f1e2f0f3863613e2492577e716e65fc1
Nicholas Porter <nicholas@metax.io>
2019-05-28 23:06:44 -0700

    Implemented the new job search by location and category

--- src/app/_services/bullhorn.service.ts
+++ src/app/_services/bullhorn.service.ts
@@ -1,18 +1,18 @@
-import {Injectable} from '@angular/core';
-import {Http, Headers, RequestOptions, Response} from '@angular/http';
-import {_URL} from "../_models/urls";
+import { Injectable } from '@angular/core';
+import { Http, Headers, RequestOptions, Response } from '@angular/http';
+import { _URL } from "../_models/urls";
 import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 
 export class BullhornService {
-    constructor(private http:Http) {}
+    constructor(private http: Http) { }
     boundaryKey = Math.random().toString(16); // random string
-    config:any = {headers:{ 'Content-Type': 'application/json'}};
+    config: any = { headers: { 'Content-Type': 'application/json' } };
 
-    parseResume(resume:any) {
+    parseResume(resume: any) {
         let promise = new Promise((resolve, reject) => {
-            this.http.post(_URL + "/bhorn/parse_resume", {resume}, this.config )
+            this.http.post(_URL + "/bhorn/parse_resume", { resume }, this.config)
                 .toPromise()
                 .then(
                     (res) => {
@@ -23,9 +23,9 @@ export class BullhornService {
         return promise
     }
 
-    attachResume(resume:any, candidate_id:any, file_type:string) {
+    attachResume(resume: any, candidate_id: any, file_type: string) {
         let promise = new Promise((resolve, reject) => {
-            this.http.post(_URL + "/bhorn/attach_resume", { resume , candidate_id, file_type }, this.config )
+            this.http.post(_URL + "/bhorn/attach_resume", { resume, candidate_id, file_type }, this.config)
                 .toPromise()
                 .then(
                     (res) => {
@@ -36,15 +36,15 @@ export class BullhornService {
         return promise
     }
 
-    getJobs(id:string) {
-        if(id) return this.getJobById(id)
-        else return this.getAllJobs()
+    getJobs(id: string) {
+        if (id) return this.getJobById(id);
+        else return this.getAllJobs();
     }
 
     //not being used currently
-    getJobById(id:string){
+    getJobById(id: string) {
         let promise = new Promise((resolve, reject) => {
-            this.http.post(_URL + "/bhorn/jobs/id", JSON.stringify({job_id:id}) ,this.config)
+            this.http.post(_URL + "/bhorn/jobs/id", JSON.stringify({ job_id: id }), this.config)
                 .toPromise()
                 .then(
                     (res) => {
@@ -55,7 +55,7 @@ export class BullhornService {
         return promise;
     }
 
-    getAllJobs(){
+    getAllJobs() {
         let promise = new Promise((resolve, reject) => {
             this.http.get(_URL + "/bhorn/jobs", this.config)
                 .toPromise()
@@ -68,11 +68,39 @@ export class BullhornService {
         return promise;
     }
 
-    submitResume(){
+    searchCategory(category: string) {
+        console.log("search category")
+        let promise = new Promise((resolve, reject) => {
+            this.http.post(_URL + "/bhorn/search_category", JSON.stringify({ category: encodeURIComponent(category) }), this.config)
+                .toPromise()
+                .then(
+                    (res) => {
+                        resolve(res);
+                    }
+                );
+        });
+        return promise;
+    }
+    
+    searchString(search_query: string) {
+        console.log("search string")
+        let promise = new Promise((resolve, reject) => {
+            this.http.post(_URL + "/bhorn/search_string", JSON.stringify({ search_query: encodeURIComponent(search_query) }), this.config)
+                .toPromise()
+                .then(
+                    (res) => {
+                        resolve(res);
+                    }
+                );
+        });
+        return promise;
+    }
+
+    submitResume() {
         return this.http.post(_URL + "/bhorn", this.config);
     }
 
-    createCandidate(data:any){
+    createCandidate(data: any) {
         let promise = new Promise((resolve, reject) => {
             this.http.put(_URL + "/bhorn/create_candidate", JSON.stringify(data), this.config)
                 .toPromise()
--- src/app/jobs/jobs.component.ts
+++ src/app/jobs/jobs.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ViewEncapsulation, OnChanges } from '@angular/core';
-import {BullhornService} from "../_services/index";
-import {Router} from '@angular/router';
+import { BullhornService } from "../_services/index";
+import { Router } from '@angular/router';
 import { Job } from "../_models/index";
 // import {data_retrieved} from "../job_description/job_description.component";
 
@@ -18,75 +18,146 @@ export let job_data: any = [];
 
 export class JobsComponent {
     constructor(private bullhornService: BullhornService,
-                private router: Router) {
+        private router: Router) {
     }
 
-    public job_data: any
+    public job_data: any = []
     selected_category: string
     selected_location: string
     categories: Array<string> = []
     locations: Array<string> = []
     loading: boolean = false
     loader: boolean = false
-    query:string
+    query: string
 
 
     ngOnInit() {
-
         if (!data_retrieved) {
             this.formatBullhornData()
         } else {
             this.loader = true
             this.job_data = job_data
-            this.createCategoriesAndLocations(job_data)
+            this.createCategoriesAndLocations(job_data);
         }
     }
 
-    onChange(category: string) {
-        // console.log("selected category: ", category)
+    public setCategory(category: string) {
+        this.selected_category = category;
     }
 
+    public setLocation(location: string) {
+        this.selected_location = location;
+    }
 
-    formatBullhornData() {
-
-        this.bullhornService.getJobs(null).then(
-            (res: any) => {
-                let jobs = JSON.parse(res._body)
-                let jd = jobs.data
-                let d
-                for (let i = 0; i < jd.length; i++) {
-                    d = jd[i].publicDescription.replace(/&nbsp;/gi,'')
-                    d = d.substring(0, 520) + "...",
-                    job_data.push({
-                            id: jd[i].id,
-                            job: new Job(
-                                jd[i].id,
-                                jd[i].title,
-                                jd[i].publicDescription,
-                                jd[i].employmentType,
-                                d,
-                                jd[i].address.city + ", " + jd[i].address.state,
-                                jd[i].address,
-                                jd[i].categories.data[0] ? jd[i].categories.data[0].name : ''
-                            )
-                        }
-                    );
-
+    searchCategory(category: string) {
+        return new Promise((resolve, reject) => {
+            this.bullhornService.searchCategory(category).then(
+                (res: any) => {
+                    resolve(this.setJobs(res, false))
+                    // this.job_data = this.setJobs(res, false)
                 }
-                this.createCategoriesAndLocations(job_data);
-                // console.log("JOBS: ", job_data);
-                this.loader = true;
-            }
-        );
+            );
+        })
+    }
+
+    searchString(query: string) {
+        console.log("Search String: ", query)
+        return new Promise((resolve, reject) => {
+
+            this.bullhornService.searchString(query).then(
+                (res: any) => {
+                    resolve(this.setJobs(res, false))
+                    // this.job_data = this.setJobs(res, false)
+                }
+            );
+        })
+    }
+
+    // Logic not ideal, but it is functional given the limited Bullhorn API 
+    public async searchJobs() {
+        if (!this.query && this.selected_category && !this.selected_location) {
+            await this.searchCategory(this.selected_category);
+            return;
+        }
+        if (this.query && this.selected_category && this.selected_location) {
+            this.searchCategory(this.selected_category).then(() => {
+                this.job_data = this.searchStringInJobsArray(this.query);
+                this.job_data = this.searchStringInJobsArray(this.selected_location);
+            })
+            return;
+        }
+        if (this.query && this.selected_category) {
+            this.searchCategory(this.selected_category).then(() => {
+                this.job_data = this.searchStringInJobsArray(this.query);
+            })
+            return;
+        }
+        if (this.query && this.selected_location) {
+            this.searchString(this.query).then(() => {
+                this.job_data = this.searchStringInJobsArray(this.selected_location);
+            })
+            return;
+        }
+        if (this.query && !this.selected_category && !this.selected_location) {
+            await this.searchString(this.query)
+            return;
+        }
+        if (!this.query && this.selected_category && this.selected_location) {
+            this.searchCategory(this.selected_category).then(()=>{
+                this.job_data = this.searchStringInJobsArray(this.selected_location);
+            })
+            return;
+        }
+        if (!this.query && !this.selected_category && this.selected_location) {
+            this.job_data = this.searchStringInJobsArray(this.selected_location);
+            return;
+        }
+    }
+
+    setJobs(res: any, setDropdowns: boolean) {
+        job_data = [];
+        let jobs = JSON.parse(res._body);
+        let jd = jobs.data;
+        let d;
+        for (let i = 0; i < jd.length; i++) {
+            d = jd[i].publicDescription.replace(/&nbsp;/gi, '')
+            d = d.substring(0, 520) + "...",
+                job_data.push({
+                    id: jd[i].id,
+                    job: new Job(
+                        jd[i].id,
+                        jd[i].title,
+                        jd[i].publicDescription,
+                        jd[i].employmentType,
+                        d,
+                        jd[i].address.city + ", " + jd[i].address.state,
+                        jd[i].address,
+                        jd[i].categories.data[0] ? jd[i].categories.data[0].name : ''
+                    )
+                });
+        }
+        if (setDropdowns) {
+            this.createCategoriesAndLocations(job_data);
+        }
+        this.loader = true;
         data_retrieved = true;
         this.job_data = job_data;
+        return this.job_data
+    }
+
+    formatBullhornData() {
+        this.bullhornService.getJobs(null).then(
+            (res: any) => {
+                this.setJobs(res, true)
+            }
+        );
     }
 
     createCategoriesAndLocations(job_data: Array<any>) {
-        this.categories.push("Category");
-        this.locations.push("Location");
+        // this.categories.push("Category");
+        // this.locations.push("Location");
         for (let i = 0; i < job_data.length; i++) {
-            if(job_data[i].job.category){
+            if (job_data[i].job.category) {
                 //only push into array if it does not already exist in array
                 if ((this.categories).indexOf(job_data[i].job.category) == -1) {
                     this.categories.push(job_data[i].job.category)
@@ -98,27 +169,41 @@ export class JobsComponent {
         }
     }
 
-    public searchStringInJobsArray() {
-        // console.log("Search");
-        if(!this.query){ this.job_data = job_data;  return;}
-        let new_job_data: Array<string>=[];
-        let query:any = [];
-            query = this.query.split(" ");
-        if(query.length <= 1) query.push(this.query);
-        for (let j = 0; j < job_data.length; j++) {
-                for(let i = 0; i < query.length; i++)
+    resetJobs() {
+        this.loader = true;
+        this.setCategory("");
+        this.setLocation("");
+        this.query = "";
+        this.createCategoriesAndLocations(job_data);
+        this.formatBullhornData();
+    }
+
+    public searchStringInJobsArray(search_query: string) {
+        console.log("Search");
+        try {
+            if (!search_query) { this.job_data = job_data; return; }
+            let new_job_data: Array<string> = [];
+            let query: any = [];
+            query = search_query.replace(",", "").split(" ");
+            if (query.length <= 1) query.push(search_query);
+            for (let j = 0; j < job_data.length; j++) {
+                for (let i = 0; i < query.length; i++)
                     if (job_data[j].job.description.toLocaleLowerCase().match(query[i].toLocaleLowerCase())
-                        || job_data[j].job.title.toLocaleLowerCase() == query[i].toLocaleLowerCase())
-                    {
+                        || job_data[j].job.title.toLocaleLowerCase() == query[i].toLocaleLowerCase()
+                        || job_data[j].job.location.toLocaleLowerCase().match(query[i].toLocaleLowerCase())) {
                         new_job_data.push(job_data[j]);
                         break;
                     }
+            }
+            if (!new_job_data.length) {
+                console.log("No match for: ", search_query);
+                this.job_data = [];
+            } else {
+                this.job_data = new_job_data;
+            }
+            return this.job_data;
+        } catch (error) {
+            console.log("Error Searching For Job: ", error)
         }
-        if(!new_job_data.length) {
-          // console.log("No match for: " , this.query);
-        }
-        this.job_data = new_job_data;
-        return -1;
     }
-
 }
